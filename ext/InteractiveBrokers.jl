6bc4da6fe43aa3270106072e83f9ed3fd7a3025c
Ande Olson <ande.olson18@gmail.com>
2024-05-02 18:42:25 -0700

    Add IB messages

--- ext/InteractiveBrokers.jl
+++ ext/InteractiveBrokers.jl
@@ -1,12 +1,13 @@
 module InteractiveBrokers
 
 using Jib
+using Lucky
 
 abstract type AbstractMsg end
 
 abstract type IBBaseMsg <: AbstractMsg end
 
-struct TickPriceMsg{A} <: IBBaseMsg
+struct TickPriceMsg{A<:TradingHours} <: IBBaseMsg
     tickerId::Int
     field::String
     price::Union{Float64,Nothing}
@@ -14,10 +15,54 @@ struct TickPriceMsg{A} <: IBBaseMsg
     attrib::Jib.TickAttrib
 end
 
-struct TickSizeMsg{A} <: IBBaseMsg
+struct TickSizeMsg{A<:TradingHours} <: IBBaseMsg
     tickerId::Int
     field::String
     size::Union{Float64,Nothing}
-  end
+end
 
+struct TickOptionMsg{A<:TradingHours} <: IBBaseMsg
+    tickerId::Int
+    tickType::String
+    tickAttrib::Int
+    impliedVol::Union{Float64,Nothing}
+    delta::Union{Float64,Nothing}
+    optPrice::Union{Float64,Nothing}
+    pvDividend::Union{Float64,Nothing}
+    gamma::Union{Float64,Nothing}
+    vega::Union{Float64,Nothing}
+    theta::Union{Float64,Nothing}
+    undPrice::Union{Float64,Nothing}
+end
+
+struct HistoricalDataMsg{A<:TradingHours} <: IBBaseMsg
+    tickerId::Int
+    dataframe::DataFrame
+end
+  
+struct SecDefOptParamsMsg{A<:TradingHours} <: IBBaseMsg
+    reqId::Int
+    exchange::String
+    underlyingConId::Int
+    tradingClass::String
+    multiplier::String
+    expirations::Vector{String}
+    strikes::Vector{Float64}
+end
+  
+struct ErrorMsg <: IBBaseMsg
+    id::Union{Int,Nothing}
+    errorCode::Union{Int,Nothing}
+    errorString::String
+    advancedOrderRejectJson::String
+end
+  
+  
+struct AccountSummaryMsg <: IBBaseMsg
+    id::I
+    account::A
+    tag::T 
+    value::V
+    currency::C
+end
 end
--- src/Utils.jl
+++ src/Utils.jl
@@ -8,6 +8,10 @@ BusinessDays.initcache(:USNYSE)
 future_trade_date(x::Int) = isbday(:USNYSE, Dates.today()) ? advancebdays(:USNYSE, Dates.today(), x) : tobday(:USNYSE, Dates.today())
 after_hours() = !isbday(:USNYSE, Dates.today()) ? true : todayat(Time(9,30), NYTZ) < now(NYTZ) < todayat(Time(16,0), NYTZ) ? false : true
 
+struct TradingHours
+    after_hours::Bool
+end
+
 
 function percentage(num::Number; rounded::Union{Nothing,Int}=nothing, humanreadable::Bool=false)
     if (humanreadable)

ece67b427f14e097285d2fd24539e7674c2f6d9d
Ande Olson <ande.olson18@gmail.com>
2024-05-12 10:16:17 -0700

    Update

--- src/Utils.jl
+++ src/Utils.jl
@@ -5,7 +5,7 @@ using BusinessDays
 using TimeZones
 
 NYTZ = tz"America/New_York"
-BusinessDays.initcache(:USNYSE)
+# BusinessDays.initcache(:USNYSE)
 future_trade_date(x::Int) = isbday(:USNYSE, Dates.today()) ? advancebdays(:USNYSE, Dates.today(), x) : tobday(:USNYSE, Dates.today())
 after_hours() = !isbday(:USNYSE, Dates.today()) ? true : todayat(Time(9,30), NYTZ) < now(NYTZ) < todayat(Time(16,0), NYTZ) ? false : true
 

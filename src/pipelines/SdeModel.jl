d890677f8273360b9bb7507b9d948eccd5355611
Ande Olson <ande.olson18@gmail.com>
2024-06-16 19:58:06 -0700

    update import

--- src/pipelines/SdeModel.jl
+++ src/pipelines/SdeModel.jl
@@ -4,7 +4,7 @@ using Lucky.Pipelines
 using Lucky.Models
 using Lucky.Config
 using Lucky.Constants
-using InteractiveBrokers
+using InteractiveBrokersExt
 
 mutable struct PreModelPipeline{S} <: AbstractPipeline
     archmodel::ArchModel{S}

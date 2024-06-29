00a504669170cbc4aa16cd834006e1970a4c2fdc
Ande Olson <ande.olson18@gmail.com>
2024-06-29 12:59:38 -0700

    fix typo

--- src/instruments/Options.jl
+++ src/instruments/Options.jl
@@ -5,4 +5,4 @@ struct Option{S,C,R,E} <: Instrument end
 Option(S::Stock, C::Type{<:Units.Currency}, R::ORDER_SIDE, E::Dates.Date) = Stock{S,C,R,E}()
 Option(S::Stock, C::Union{Symbol, AbstractString}, R::ORDER_SIDE, E::Dates.Date) = Stock{S,Units.CurrencyType(C),R,E}()
 
-Units.currency(::Optoin{S,C,R,E}) where {S,C,R,E} = Units.CurrencyType(C)
+Units.currency(::Option{S,C,R,E}) where {S,C,R,E} = Units.CurrencyType(C)

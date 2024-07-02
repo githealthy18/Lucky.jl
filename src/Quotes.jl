99b5cec7970a9e1b87e3e8cf46ba09cb914a4f23
Ande Olson <ande.olson18@gmail.com>
2024-07-01 20:24:00 -0700

    Update ticks

--- src/Quotes.jl
+++ src/Quotes.jl
@@ -14,18 +14,13 @@ using Dates
 using Statistics
 
 abstract type AbstractQuote end
-abstract type AbstractTick end
 
 QuoteType(I::Type{<:Instrument}, params...) = error("You probably forgot to implement QuoteType(::$(I), $(params...))")
 QuoteType(Q::Type{<:AbstractQuote}) = Q
 Units.TimestampType(Q::Type{<:AbstractQuote}) = error("You probably forgot to implement TimestampType(::$(Q))")
 
-TickType(T::Type{<:AbstractTick}, params...) = error("You probably forgot to implement TickType(::$(T), $(params...))")
-TickType(::T) where {T<:AbstractTick} = T
-
-struct PriceQuote{I,T,Q,S,D} <: AbstractQuote
+struct PriceQuote{I,Q,S,D} <: AbstractQuote
     instrument::I
-    tick::T
     price::Q
     size::S
     timestamp::D
@@ -43,15 +38,15 @@ struct VolumeQuote{I,Q,D} <: AbstractQuote
 end
 
 # Interfaces
-Quote(instrument::Instrument, tick::T, price::Q, size::S, stamp::D) where {T<:AbstractTick,Q<:Union{Nothing,Number},S<:Union{Nothing,Number},D<:Dates.AbstractTime} = PriceQuote(instrument, tick, price, size, stamp)
+Quote(instrument::Instrument, price::Q, size::S, stamp::D) where {Q<:Union{Nothing,Number},S<:Union{Nothing,Number},D<:Dates.AbstractTime} = PriceQuote(instrument, price, size, stamp)
 Quote(instrument::Instrument, ohlc::Q) where {Q<:Ohlc} = OhlcQuote(instrument, ohlc)
 Quote(instrument::Instrument, volume::Q, stamp::D) where {Q<:Number,D<:Dates.AbstractTime} = VolumeQuote(instrument, volume, stamp)
 
 QuoteType(I::Type{<:Instrument}, Q::Type{<:Ohlc}) = OhlcQuote{I,Q}
-QuoteType(I::Type{<:Instrument}, T::Type{<:AbstractTick}, P::Type{<:Number}=Float64, S::Type{<:Number}=Float64, D::Type{<:Dates.AbstractTime}=Dates.DateTime) = PriceQuote{I,T,P,S,D}
+QuoteType(I::Type{<:Instrument}, P::Type{<:Number}=Float64, S::Type{<:Number}=Float64, D::Type{<:Dates.AbstractTime}=Dates.DateTime) = PriceQuote{I,P,S,D}
 QuoteType(I::Type{<:Instrument}, V::Type{<:Number}, D::Type{<:Dates.AbstractTime}=Dates.DateTime) = VolumeQuote{I,V,D}
 QuoteType(i::Instrument, Q::Type{<:Ohlc}) = QuoteType(InstrumentType(i), Q)
-QuoteType(i::Instrument, T::Type{<:AbstractTick}, P::Type{<:Number}=Float64, S::Type{<:Number}=Float64, D::Type{<:Dates.AbstractTime}=DateTime) = QuoteType(InstrumentType(i), T, P, S, D)
+QuoteType(i::Instrument, P::Type{<:Number}=Float64, S::Type{<:Number}=Float64, D::Type{<:Dates.AbstractTime}=DateTime) = QuoteType(InstrumentType(i), P, S, D)
 QuoteType(i::Instrument, V::Type{<:Number}, D::Type{<:Dates.AbstractTime}=DateTime) = QuoteType(InstrumentType(i), V, D)
 
 Units.currency(q::AbstractQuote) = Units.currency(q.instrument)
@@ -59,7 +54,7 @@ timestamp(q::OhlcQuote) = q.ohlc.timestamp
 timestamp(q::PriceQuote) = q.timestamp
 
 Units.TimestampType(::Type{<:OhlcQuote{I,O}}) where {I,O} = Units.TimestampType(O)
-Units.TimestampType(::Type{<:PriceQuote{I,T,P,S,D}}) where {I,T,P,S,D} = D
+Units.TimestampType(::Type{<:PriceQuote{I,P,S,D}}) where {I,P,S,D} = D
 
 import Base: +, -, *, /, convert, isless
 # https://github.com/JuliaLang/julia/blob/0a8916446b782eae1a09681b2b47c1be26fab7f3/base/missing.jl#L119
@@ -70,10 +65,10 @@ for f in (:(+), :(-)) #, :(*), :(/), :(^), :(mod), :(rem))
     end
 end
 
-+(x::I, y::I) where {I<:PriceQuote} = I(x.instrument, x.tick, x.price + y.price, mean((x.size, y.size)), max(timestamp(x), timestamp(y)))
--(x::I, y::I) where {I<:PriceQuote} = I(x.instrument, x.tick, x.price - y.price, mean((x.size, y.size)), max(timestamp(x), timestamp(y)))
-*(x::I, y::N) where {I<:PriceQuote,N<:Number} = I(x.instrument, x.tick, x.price * y, x.size, timestamp(x))
-/(x::I, y::N) where {I<:PriceQuote,N<:Number} = I(x.instrument, x.tick, x.price / y, x.size, timestamp(x))
++(x::I, y::I) where {I<:PriceQuote} = I(x.instrument, x.price + y.price, mean((x.size, y.size)), max(timestamp(x), timestamp(y)))
+-(x::I, y::I) where {I<:PriceQuote} = I(x.instrument, x.price - y.price, mean((x.size, y.size)), max(timestamp(x), timestamp(y)))
+*(x::I, y::N) where {I<:PriceQuote,N<:Number} = I(x.instrument, x.price * y, x.size, timestamp(x))
+/(x::I, y::N) where {I<:PriceQuote,N<:Number} = I(x.instrument, x.price / y, x.size, timestamp(x))
 convert(T::Type{<:Number}, x::PriceQuote) = convert(T, x.price)
 isless(x::I, y::I) where {I<:PriceQuote} = isless(x.price, y.price)
 

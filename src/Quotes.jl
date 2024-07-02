2882cef911b66accfc95c6877aaaa27e4dac5626
Ande Olson <ande.olson18@gmail.com>
2024-07-02 12:17:37 -0700

    Update type definition

--- src/Quotes.jl
+++ src/Quotes.jl
@@ -43,10 +43,10 @@ Quote(instrument::Instrument, volume::Q, stamp::D) where {Q<:Number,D<:Dates.Abs
 
 QuoteType(I::Type{<:Instrument}, Q::Type{<:Ohlc}) = OhlcQuote{I,Q}
 QuoteType(I::Type{<:Instrument}, P::Type{<:Number}=Float64, S::Type{<:Number}=Float64, D::Type{<:Dates.AbstractTime}=Dates.DateTime) = PriceQuote{I,P,S,D}
-QuoteType(I::Type{<:Instrument}, V::Type{<:Number}, D::Type{<:Dates.AbstractTime}=Dates.DateTime) = VolumeQuote{I,V,D}
+QuoteType(I::Type{<:Instrument}, V::Type{<:Number}=Float64, D::Type{<:Dates.AbstractTime}=Dates.DateTime) = VolumeQuote{I,V,D}
 QuoteType(i::Instrument, Q::Type{<:Ohlc}) = QuoteType(InstrumentType(i), Q)
 QuoteType(i::Instrument, P::Type{<:Number}=Float64, S::Type{<:Number}=Float64, D::Type{<:Dates.AbstractTime}=DateTime) = QuoteType(InstrumentType(i), P, S, D)
-QuoteType(i::Instrument, V::Type{<:Number}, D::Type{<:Dates.AbstractTime}=DateTime) = QuoteType(InstrumentType(i), V, D)
+QuoteType(i::Instrument, V::Type{<:Number}=Float64, D::Type{<:Dates.AbstractTime}=DateTime) = QuoteType(InstrumentType(i), V, D)
 
 Units.currency(q::AbstractQuote) = Units.currency(q.instrument)
 timestamp(q::OhlcQuote) = q.ohlc.timestamp

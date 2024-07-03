55622b2a18e8e632d29fe093c219801971ea8d2c
Ande Olson <ande.olson18@gmail.com>
2024-07-02 20:30:58 -0700

    Update

--- ext/InteractiveBrokersExt.jl
+++ ext/InteractiveBrokersExt.jl
@@ -193,7 +193,7 @@ function Lucky.feed(client::InteractiveBrokersObservable, instr::Instrument, ::V
     lastSize = lastSizeSubject |> with_latest(tickStringSubject) |> Rocket.map(Lucky.VolumeQuote, merge_lastSize)
 
     setTimeout(timeout) do 
-        println("ENDING")
+        println("$(client.requestMappings[CallbackKey(requestId, :tickPrice, InteractiveBrokers.TickTypes.LAST)].live)")
         if client.requestMappings[CallbackKey(requestId, :tickPrice, InteractiveBrokers.LAST)].live
             Lucky.end_feed(client, instr, Val(:livedata))
         end

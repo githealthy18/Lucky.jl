acff1538b511fc7a61ce960393d3b874eae9a85b
Ande Olson <ande.olson18@gmail.com>
2024-07-06 12:17:17 -0700

    add utils

--- examples/prod.jl
+++ examples/prod.jl
@@ -3,14 +3,18 @@ using Lucky
 using Rocket
 using DataFrames
 using InteractiveBrokers
+using BusinessDays
 using Lucky.Quotes: Last, Bid, Ask, Mark, High, Low, Close, Open, Volume, AskSize, BidSize, LastSize
+using Lucky.Utils: after_hours
+
+BusinessDays.initcache(:USNYSE)
 
 mutable struct PreModel{A} <: AbstractStrategy
     data::DataFrame
     open::Union{Missing, Lucky.PriceQuote{I,Open,P,S,D} where {I,P,S,D}}
     high::Union{Missing,Lucky.PriceQuote{I,High,P,S,D} where {I,P,S,D}}
     low::Union{Missing, Lucky.PriceQuote{I,Low,P,S,D} where {I,P,S,D}}
-    close::Union{Missing, Lucky.PriceQuote{I,Close,P,S,D} where {I,P,S,D}}
+    close::Union{Missing, Lucky.PriceQuote{I,Close,P,S,D} where {I,P,S,D}, Lucky.PriceQuote{I,Mark,P,S,D} where {I,P,S,D}}
     volume::Union{Missing, Lucky.PriceQuote{I,Volume,P,D} where {I,P,S,D}}
     next::A
 end
@@ -19,6 +23,10 @@ PreModel(next::A) where {A} = PreModel(DataFrame(), missing, missing, missing, m
 
 function Rocket.on_next!(strat::PreModel, data::DataFrame)
     strat.data = data
+end
+
+function Rocket.on_next!(strat::PreModel, data::Lucky.PriceQuote{I,T,P,S,D}) where {I,T<:Mark,P,S,D}
+    strat.close = data
 end
 
 function Rocket.on_next!(strat::PreModel, data::Lucky.PriceQuote{I,T,P,S,D}) where {I,T,P,S,D}
@@ -46,12 +54,13 @@ end
 # end
 
 function Rocket.on_complete!(strat::PreModel)
-    println("Completed")
-    strat.data.high[end] = strat.high.price
-    strat.data.low[end] = strat.low.price
-    strat.data.close[end] = strat.close.price
-    strat.data.open[end] = strat.open.price
-    strat.data.volume[end] = strat.volume.volume
+    if !after_hours()
+        strat.data.high[end] = strat.high.price
+        strat.data.low[end] = strat.low.price
+        strat.data.close[end] = strat.close.price
+        strat.data.open[end] = strat.open.price
+        strat.data.volume[end] = strat.volume.volume
+    end
     complete!(strat.next)
 end
 
@@ -61,7 +70,7 @@ stock = Stock(:AAPL,:USD)
 actor = PreModel(lambda(DataFrame; on_complete = ()->println("Done!")))
 hist = Lucky.feed(client, stock, Val(:historicaldata))
 feeds = Lucky.feed(client, stock, Val(:livedata))
-source = merged((hist |> first(), feeds.openPrice |> first(), feeds.highPrice |> first(), feeds.lowPrice |> first(), feeds.closePrice |> first(), feeds.volume |> first()))
+source = merged((hist |> first(), feeds.openPrice |> first(), feeds.highPrice |> first(), feeds.lowPrice |> first(), feeds.markPrice |> first(), feeds.volume |> first()))
 subscribe!(source, actor)
 
 mutable struct GoldenCross{A} <: AbstractStrategy
--- src/Utils.jl
+++ src/Utils.jl
@@ -1,6 +1,22 @@
 module Utils
 
 using Dictionaries
+using Dates
+using BusinessDays
+using TimeZones
+
+NYTZ = tz"America/New_York"
+future_trade_date(x::Int) = isbday(:USNYSE, Dates.today()) ? advancebdays(:USNYSE, Dates.today(), x) : tobday(:USNYSE, Dates.today())
+after_hours() = !isbday(:USNYSE, Dates.today()) ? true : todayat(Time(9,30), NYTZ) < now(NYTZ) < todayat(Time(16,0), NYTZ) ? false : true
+
+function assign_businessday(data)
+	output = Vector{Float32}(undef, length(data))
+	for i in eachindex(data)
+		output[i] = isbday(BusinessDays.USNYSE(), Date(data[i]))
+	end
+	return output
+end
+
 
 function percentage(num::Number; rounded::Union{Nothing,Int}=nothing, humanreadable::Bool=false)
     if (humanreadable)

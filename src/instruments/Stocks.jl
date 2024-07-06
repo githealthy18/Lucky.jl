04061e2dacf3d9e9d18ae8d9b2085ee0412870ce
Ande Olson <ande.olson18@gmail.com>
2024-07-06 13:47:16 -0700

    Update

--- src/instruments/Stocks.jl
+++ src/instruments/Stocks.jl
@@ -5,7 +5,7 @@ struct Stock{S,C} <: Instrument end
 Stock(S::Symbol, C::Type{<:Units.Currency}) = Stock{S,C}()
 Stock(S::Symbol, C::Union{Symbol, AbstractString}) = Stock{S,Units.CurrencyType(C)}()
 
-symbol(::T) where {S,C,T<:Stock{S,C}} = String(S)
-symbol(::Type{<:Stock{S,C}}) where {S,C} = String(S)
+Units.symbol(::T) where {S,C,T<:Stock{S,C}} = String(S)
+Units.symbol(::Type{<:Stock{S,C}}) where {S,C} = String(S)
 
 Units.currency(::Stock{S,C}) where {S,C} = Units.CurrencyType(C)

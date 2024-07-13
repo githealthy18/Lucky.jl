d99ece1cffb1e94dee2852a7662c0c2793165d05
Ande Olson <ande.olson18@gmail.com>
2024-07-13 15:23:48 -0700

    debug

--- ext/InteractiveBrokersExt.jl
+++ ext/InteractiveBrokersExt.jl
@@ -233,7 +233,7 @@ function Lucky.feed(client::InteractiveBrokersObservable, instr::Instrument, ::V
     insert!(client.mergedCallbacks, Pair(instr, :livedata), output)
 
     setTimeout(timeout) do 
-        println("here")
+        println(haskey(client.mergedCallbacks, Pair(instr, :livedata)) || Rocket.isactive(output))
         if haskey(client.mergedCallbacks, Pair(instr, :livedata)) || Rocket.isactive(output)
             Lucky.end_feed(client, instr, Val(:livedata))
         end

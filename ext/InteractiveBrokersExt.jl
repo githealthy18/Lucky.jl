c93a97fe7068ca61426d64f1d50e6034f1cb306e
Ande Olson <ande.olson18@gmail.com>
2024-07-15 17:47:23 -0700

    thread scheduling

--- ext/InteractiveBrokersExt.jl
+++ ext/InteractiveBrokersExt.jl
@@ -177,23 +177,23 @@ function Lucky.feed(client::InteractiveBrokersObservable, instr::Instrument, ::V
 
     # TODO callbacks depending on requested data
 
-    lastPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=AsyncScheduler()))
+    lastPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=ThreadsScheduler()))
 
-    bidPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=AsyncScheduler()))
-    askPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=AsyncScheduler()))
-    markPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=AsyncScheduler()))
+    bidPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=ThreadsScheduler()))
+    askPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=ThreadsScheduler()))
+    markPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=ThreadsScheduler()))
 
-    highPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=AsyncScheduler()))
-    lowPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=AsyncScheduler()))
-    openPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=AsyncScheduler()))
-    closePriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=AsyncScheduler()))
+    highPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=ThreadsScheduler()))
+    lowPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=ThreadsScheduler()))
+    openPriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=ThreadsScheduler()))
+    closePriceSubject = RecentSubject(Lucky.PriceQuote, Subject(Lucky.PriceQuote; scheduler=ThreadsScheduler()))
 
-    volumeSubject = RecentSubject(Lucky.VolumeQuote, Subject(Lucky.VolumeQuote, scheduler=AsyncScheduler()))
-    askSizeSubject = RecentSubject(Lucky.VolumeQuote, Subject(Lucky.VolumeQuote, scheduler=AsyncScheduler()))
-    bidSizeSubject = RecentSubject(Lucky.VolumeQuote, Subject(Lucky.VolumeQuote, scheduler=AsyncScheduler()))
-    lastSizeSubject = RecentSubject(Lucky.VolumeQuote, Subject(Lucky.VolumeQuote, scheduler=AsyncScheduler()))
+    volumeSubject = RecentSubject(Lucky.VolumeQuote, Subject(Lucky.VolumeQuote, scheduler=ThreadsScheduler()))
+    askSizeSubject = RecentSubject(Lucky.VolumeQuote, Subject(Lucky.VolumeQuote, scheduler=ThreadsScheduler()))
+    bidSizeSubject = RecentSubject(Lucky.VolumeQuote, Subject(Lucky.VolumeQuote, scheduler=ThreadsScheduler()))
+    lastSizeSubject = RecentSubject(Lucky.VolumeQuote, Subject(Lucky.VolumeQuote, scheduler=ThreadsScheduler()))
 
-    tickStringSubject = RecentSubject(DateTime, Subject(DateTime; scheduler=AsyncScheduler()))
+    tickStringSubject = RecentSubject(DateTime, Subject(DateTime; scheduler=ThreadsScheduler()))
 
     insert!(client.requestMappings, CallbackKey(requestId, :tickPrice, InteractiveBrokers.TickTypes.LAST), CallbackValue(tickPrice, lastPriceSubject, instr))
 
@@ -268,7 +268,7 @@ function Lucky.feed(client, instr::Instrument, ::Val{:historicaldata}; timeout=6
 
     InteractiveBrokers.reqHistoricalData(client, requestId, instr, "", "3 Y", "1 day", "TRADES" ,false, 1, false)
 
-    historicalDataSubject = RecentSubject(DataFrame, Subject(DataFrame; scheduler=AsyncScheduler()))
+    historicalDataSubject = RecentSubject(DataFrame, Subject(DataFrame; scheduler=ThreadsScheduler()))
     insert!(client.requestMappings, CallbackKey(requestId, :historicalData, nothing), CallbackValue(historicalData, historicalDataSubject, instr))
     insert!(client.mergedCallbacks, Pair(instr, :historicaldata), historicalDataSubject)
 
@@ -331,7 +331,7 @@ function Lucky.feed(client::InteractiveBrokersObservable, instr::Instrument, ::V
 
     InteractiveBrokers.reqContractDetails(client, requestId, instr)
 
-    contractDetailsSubject = RecentSubject(InteractiveBrokers.ContractDetails, Subject(InteractiveBrokers.ContractDetails; scheduler=AsyncScheduler()))
+    contractDetailsSubject = RecentSubject(InteractiveBrokers.ContractDetails, Subject(InteractiveBrokers.ContractDetails; scheduler=ThreadsScheduler()))
     insert!(client.requestMappings, CallbackKey(requestId, :contractDetails, nothing), CallbackValue(contractDetails, contractDetailsSubject, instr))
     insert!(client.mergedCallbacks, Pair(instr, :contractDetails), contractDetailsSubject)
 

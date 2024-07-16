6bc6be381f6c534f03aaf88219be71c1da2431c3
Ande Olson <ande.olson18@gmail.com>
2024-07-16 07:24:37 -0700

    Add convert

--- src/Constants.jl
+++ src/Constants.jl
@@ -6,7 +6,8 @@ export OPTION_RIGHT, CALL, PUT
 @enum ORDER_SIDE BUY_SIDE=1 SELL_SIDE=-1
 @enum OPTION_RIGHT CALL=1 PUT=-1
 
-import Base: String
+import Base: String, convert
 String(R::OPTION_RIGHT) = R == CALL ? "C" : "P"
+convert(T::Type{Bool}, R::OPTION_RIGHT) = R == CALL ? true : false
 
 end

e901b746750ae819e842d370c2ac19149fcebc5a
Ande Olson <ande.olson18@gmail.com>
2024-08-01 20:42:54 -0700

    add tag

--- ext/InteractiveBrokers/Callbacks.jl
+++ ext/InteractiveBrokers/Callbacks.jl
@@ -134,7 +134,7 @@ function historicalData(ib::InteractiveBrokersObservable, reqId::Int, bar::DataF
 end
 
 function accountSummary(ib::InteractiveBrokersObservable, reqId::Int, account::String, tag::String, value::String, currency::String)
-    @info value
+    @info tag
     key = CallbackKey(reqId, :accountSummary, nothing)
     if haskey(ib.requestMappings, key)
         val = ib.requestMappings[key]

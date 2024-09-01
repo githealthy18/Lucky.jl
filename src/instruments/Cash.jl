18bd2e23c3479498784471774982a4b1446fe833
Ande Olson <ande.olson18@gmail.com>
2024-09-01 15:43:12 -0700

    remove units ref

--- src/instruments/Cash.jl
+++ src/instruments/Cash.jl
@@ -3,10 +3,10 @@ export Cash
 # TODO Improvement: have Cash made a singleton
 struct Cash{C} <: Instrument end
 
-@inline Cash(C::Symbol) = Cash{Units.Currency{C}}()
+@inline Cash(C::Symbol) = Cash{Currency{C}}()
 @inline Cash(s::String) = Cash(Symbol(s))
 
-currency(::Cash{C}) where {C<:Units.Currency} = C
-currency(::Type{Cash{C}}) where {C<:Units.Currency} = C
+currency(::Cash{C}) where {C<:Currency} = C
+currency(::Type{Cash{C}}) where {C<:Currency} = C
 
 symbol(::T) where {C,T<:Cash{C}} = String(C)
--- src/instruments/Stocks.jl
+++ src/instruments/Stocks.jl
@@ -2,12 +2,12 @@ export Stock
 
 using AutoHashEquals
 
-@auto_hash_equals cache=true struct Stock{S,C<:Units.Currency} <: Instrument 
+@auto_hash_equals cache=true struct Stock{S,C<:Currency} <: Instrument 
     symbol::S
     currency::C
 end
 
-Stock(symbol::Symbol, currency::Union{AbstractString, Symbol}) = Stock(symbol, Units.Currency(currency))
+Stock(symbol::Symbol, currency::Union{AbstractString, Symbol}) = Stock(symbol, Currency(currency))
 
 symbol(stock::T) where {S,C,T<:Stock{S,C}} = String(stock.symbol)
 currency(stock::Stock{S,C}) where {S,C} = String(stock.currency)

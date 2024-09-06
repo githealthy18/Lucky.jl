90951a8572f47fedabe1bf8d766364eee6ad3625
Ande Olson <ande.olson18@gmail.com>
2024-09-05 20:18:04 -0700

    add validid

--- src/Exchanges.jl
+++ src/Exchanges.jl
@@ -1,7 +1,9 @@
-export AbstractExchange, exchange, placeorder
+export AbstractExchange, exchange, placeorder, nextValidId
 
 @inline exchange(s::Symbol, params...) = exchange(Val(s), params...)
 
 abstract type AbstractExchange <: Actor{Any} end
 
 placeorder(x::Any) = error("You probably forgot to implement placeorder($(x))")
+
+function nextValidId end

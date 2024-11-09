0b4bb6155fdde0485d94e2ae4474dc42cc9a97e4
Ande Olson <ande.olson18@gmail.com>
2024-11-09 07:54:06 -0800

    update

--- test/test_orders.jl
+++ test/test_orders.jl
@@ -29,12 +29,12 @@
     @testset "Algorithmic Market Orders" begin
         @testset "Constructors" begin
             instr = Cash(:USD)
-            order = AlgorithmicMarketOrder(instr, CLOSE, SELL_SIDE, 100, "algo", now())
+            order = AlgorithmicMarketOrder(instr, CLOSE, SELL_SIDE, 100, "algo", (adaptivePriority="Urgent",), now())
             @test order isa AlgorithmicMarketOrder{InstrumentType(instr), ORDER_SIDE, Int, DateTime}
         end
         @testset "Interfaces" begin
             instr = Cash(:USD)
-            order = AlgorithmicMarketOrder(instr, CLOSE, SELL_SIDE, 100, "algo", now())
+            order = AlgorithmicMarketOrder(instr, CLOSE, SELL_SIDE, 100, "algo", (adaptivePriority="Urgent",), now())
             @test currency(order) == CurrencyType(currency(instr))
             @test currency(OrderType(order)) == CurrencyType(currency(instr))
         end
@@ -42,12 +42,12 @@
     @testset "Algorithmic Limit Orders" begin
         @testset "Constructors" begin
             instr = Cash(:USD)
-            order = AlgorithmicLimitOrder(instr, CLOSE, SELL_SIDE, 100, 1.0889, "algo", now())
+            order = AlgorithmicLimitOrder(instr, CLOSE, SELL_SIDE, 100, 1.0889, "algo", (adaptivePriority="Urgent",), now())
             @test order isa AlgorithmicLimitOrder{InstrumentType(instr), ORDER_SIDE, Int, DateTime}
         end
         @testset "Interfaces" begin
             instr = Cash(:USD)
-            order = AlgorithmicLimitOrder(instr, CLOSE, SELL_SIDE, 100, 1.0889, "algo", now())
+            order = AlgorithmicLimitOrder(instr, CLOSE, SELL_SIDE, 100, 1.0889, "algo", (adaptivePriority="Urgent",), now())
             @test currency(order) == CurrencyType(currency(instr))
             @test currency(OrderType(order)) == CurrencyType(currency(instr))
         end
